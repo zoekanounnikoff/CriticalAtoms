d502162be6e2f33d058d4b25e8ae7e84902bc7ba
Zoe Kanounnikoff <zoe.kanounnikoff@student-cs.fr>
2020-06-29 15:45:51 +0200

    ça avance

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
+import tkinter as tk
+
 class Case:
+
+    CASES = []
 
     def __init__(self, numberAtoms):
         self.numberAtoms = numberAtoms
+        self.isBorder = False
+        self.isCorner = False
 
     def addAtom(self):
-        if self.numberAtoms < 3:
-            self.numberAtoms += 1
+        if self.isCorner:
+            if self.numberAtoms < 2: self.numberAtoms += 1
+            else: self.split()
+        elif self.isBorder:
+            if self.numberAtoms < 3: self.numberAtoms += 1
+            else: self.split()
+        else:
+            if self.numberAtoms < 4: self.numberAtoms += 1
+            else: self.split()
+
+    @classmethod
+    def initializeCases(cls, taille):
+        cls.CASES = [[Case(0) for i in range(taille)] for j in range(taille)]
+        cls.CASES[0][0] = Corner(0)
+        cls.CASES[0][taille-1] = Corner(0)
+        cls.CASES[taille-1][0] = Corner(0)
+        cls.CASES[taille-1][taille-1] = Corner(0)
+
+    
+    def split(self):
+        pass
 
 class Border(Case):
+
     def __init__(self, numberAtoms):
-        super().__init__()
+        super().__init__(numberAtoms)
+        self.isBorder = True
+        self.isCorner = False
+class Corner(Case):
 
-class Corner(Border):
-    def __init__():
-        
+    def __init__(self, numberAtoms):
+        super().__init__(numberAtoms)
+        self.isCorner = True
+        self.isBorder = False
 
-maCase = Case(3)
-print(maCase.numberAtoms)
+
+def main():
+    Case.initializeCases(10)
+    print(Case.CASES)
+
+main()
