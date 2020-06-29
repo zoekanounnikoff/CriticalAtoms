6911b3b47cc64997d49a13d2ec708b9904718679
Zoe Kanounnikoff <zoe.kanounnikoff@student-cs.fr>
2020-06-29 15:26:18 +0200

    création des premières classes

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 class Case:
-    def __init__(self):
-        pass
 
+    def __init__(self, numberAtoms):
+        self.numberAtoms = numberAtoms
+
+    def addAtom(self):
+        if self.numberAtoms < 3:
+            self.numberAtoms += 1
+
+class Border(Case):
+    def __init__(self, numberAtoms):
+        super().__init__()
+
+class Corner(Border):
+    def __init__():
+        
+
+maCase = Case(3)
+print(maCase.numberAtoms)
